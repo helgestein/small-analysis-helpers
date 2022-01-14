374fd63d868076f3c6f48a48e9a35067c280f54d
helgestein <helge.stein@kit.edu>
2022-01-14 14:25:22 +0100

    Update hexagon.py
    way shorter and more elegant version

--- hexagon.py
+++ hexagon.py
@@ -1,34 +1,11 @@
+#better version:
 import itertools as it
 import matplotlib.pyplot as plt
-
-comps = np.array([i for i in it.product([i/3 for i in range(4)],repeat=3) if sum(i)==1])
-
-cartxs = 1.-comps[:, 0]-comps[:, 1]/2.
-cartys = np.sqrt(3) * comps[:, 1] / 2.0
-
-#overlaps
-plt.scatter(cartxs,cartys)
-plt.scatter(cartxs,-cartys)
-plt.scatter(-cartxs,cartys)
-plt.scatter(-cartxs,-cartys)
-plt.scatter(cartxs-0.5,cartys-np.sqrt(3)/2)
-plt.scatter(cartxs-0.5,-(cartys-np.sqrt(3)/2))
-
-cx = [c for c in cartxs]
-cy = [c for c in cartys]
-cx.extend([c for c in cartxs])
-cy.extend([-c for c in cartys])
-
-cx.extend([-c for c in cartxs])
-cy.extend([c for c in cartys])
-
-cx.extend([-c for c in cartxs])
-cy.extend([-c for c in cartys])
-
-cx.extend([c-0.5 for c in cartxs])
-cy.extend([c-np.sqrt(3)/2 for c in cartys])
-
-cx.extend([c-0.5 for c in cartxs])
-cy.extend([-(c-np.sqrt(3)/2) for c in cartys])
-
-uni = np.unique(np.array([cx,cy]),axis=1)
+import numpy as np
+x2,y2 = 1,0
+x3,y3 = 0.5,np.sqrt(2)/2
+p1,p2 = np.array([x2,y2]),np.array([x3,y3])
+pos = np.array([p1*n1+p2*n2 for n1,n2 in it.product([-3+i for i in range(7)],repeat=2) if abs(n1+n2)<=3])
+pos = np.unique(pos)
+plt.scatter(pos[:,0],pos[:,1])
+plt.axis('equal')
